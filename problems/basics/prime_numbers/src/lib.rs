4b3b142c5037bfbf48879caabf90a1ccf5303878
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-13 23:32:39 +0300

    Automatic message by rover-submit

--- problems/basics/prime_numbers/src/lib.rs
+++ problems/basics/prime_numbers/src/lib.rs
@@ -1,6 +1,19 @@
 #![forbid(unsafe_code)]
 
 pub fn get_n_prime_numbers(n: u32) -> Vec<u32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut sieve = vec![1; usize::try_from(n + 1).unwrap()];
+    let mut ans = Vec::new();
+    for i in 2..n {
+        if sieve[i as usize] == 1 {
+            ans.push(i);
+            for j in (i * i..n).step_by(usize::try_from(n + 1).unwrap()) {
+                sieve[j as usize] = 0;
+
+            }
+        }
+    };
+    ans
+
+
+
 }
