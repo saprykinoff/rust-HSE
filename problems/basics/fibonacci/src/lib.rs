ebf7ff8d84f0457bc48b425d25b35859cd37a0e6
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-13 22:50:47 +0300

    Automatic message by rover-submit

--- problems/basics/fibonacci/src/lib.rs
+++ problems/basics/fibonacci/src/lib.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 
 pub fn get_nth_fibonacci(n: u32) -> u32 {
-    // TODO: your code goes here.
-    unimplemented!()
+    if n < 1 {return 0;}
+    if n == 1 {return 1;}
+    get_nth_fibonacci(n - 1) + get_nth_fibonacci(n - 2)
 }
