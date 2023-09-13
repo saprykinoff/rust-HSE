1cfc130648c48444c2cc53a9b9d7cac21f4561ca
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-13 23:44:25 +0300

    Automatic message by rover-submit

--- problems/basics/prime_numbers/src/lib.rs
+++ problems/basics/prime_numbers/src/lib.rs
@@ -1,15 +1,20 @@
 #![forbid(unsafe_code)]
 
+fn is_prime(n:u32) -> bool {
+    for i in 2..(n as f64).sqrt() as u32 + 1 {
+        if n % i == 0 {return false;}
+    }
+    true
+}
+
 pub fn get_n_prime_numbers(n: u32) -> Vec<u32> {
-    let mut sieve = vec![1; usize::try_from(n + 1).unwrap()];
+    let mut cur = 2;
     let mut ans = Vec::new();
-    for i in 2..n {
-        if sieve[i as usize] == 1 {
-            ans.push(i);
-            for j in (i * i..n).step_by(usize::try_from(n + 1).unwrap()) {
-                sieve[j as usize] = 0;
-            }
+    while ans.len() < n as usize {
+        if is_prime(cur) {
+            ans.push(cur);
         }
+        cur += 1;
     }
     ans
 }
