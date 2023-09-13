37ec26089f3e0fc090fbeab023846c46a80c02c0
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-13 23:15:37 +0300

    Automatic message by rover-submit

--- problems/basics/palindrome/src/lib.rs
+++ problems/basics/palindrome/src/lib.rs
@@ -5,7 +5,7 @@ pub fn is_palindrome(number: u32) -> bool {
     let mut y: u64 = 0;
     while x > 0 {
         let d = x % 10;
-        x = x / 10;
+        x /= 10;
         y = y * 10 + d as u64;
     }
     y == number as u64
