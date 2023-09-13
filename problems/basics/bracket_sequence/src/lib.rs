964ea69f9790d75136c31c0b4cdf8cb9a041ffe3
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-13 22:39:53 +0300

    Automatic message by rover-submit

--- problems/basics/bracket_sequence/src/lib.rs
+++ problems/basics/bracket_sequence/src/lib.rs
@@ -1,6 +1,21 @@
 #![forbid(unsafe_code)]
 
 pub fn is_correct_bracket_sequence(s: &str) -> bool {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut stack = Vec::new();
+    for ch in s.chars() {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            stack.push(ch);
+        } else {
+            let lst = stack.pop();
+            if (ch == ')' && lst == Some('(')
+                || ch == ']' && lst == Some('[')
+                || ch == '}' && lst == Some('{'))
+            {
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.len() == 0;
 }
