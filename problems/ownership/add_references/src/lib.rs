001f63184015d861599ec509443af053f2ffbd43
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-21 19:10:52 +0300

    Automatic message by rover-submit

--- problems/ownership/add_references/src/lib.rs
+++ problems/ownership/add_references/src/lib.rs
@@ -8,44 +8,45 @@ pub struct Statistics {
     pub max: i32,
 }
 
-fn calculate_average(data: Vec<i32>) -> i32 {
+fn calculate_average(data: &Vec<i32>) -> i32 {
     let mut sum = 0;
     let mut cnt = 0;
     for x in data {
         sum += x;
         cnt += 1;
     }
-    return sum / cnt;
+    sum / cnt
 }
 
-fn calculate_median(data: Vec<i32>) -> i32 {
+fn calculate_median(data: &mut Vec<i32>) -> i32 {
     data.sort();
-    return data[data.len() / 2];
+    data[data.len() / 2]
 }
 
-fn calculate_minmax(data: Vec<i32>) -> (i32, i32) {
+fn calculate_minmax(data: &[i32]) -> (i32, i32) {
     let mut min = data[0];
     let mut max = data[0];
     for x in data {
-        if min > x {
-            min = x;
+        if min > *x {
+            min = *x;
         }
-        if max < x {
-            max = x;
+        if max < *x {
+            max = *x;
         }
     }
-    return (min, max);
+    (min, max)
 }
 
-pub fn calculate_statistics(data: Vec<i32>) -> Statistics {
+pub fn calculate_statistics(data: &Vec<i32>) -> Statistics {
     let average = calculate_average(data);
-    let median = calculate_median(data);
+    let mut data_med = data.clone();
+    let median = calculate_median(&mut data_med);
     let (min, max) = calculate_minmax(data);
 
-    return Statistics {
+    Statistics {
         average,
         median,
         min,
         max,
-    };
+    }
 }
