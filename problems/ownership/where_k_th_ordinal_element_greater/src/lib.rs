9cedaac80e90ead5a269ff1b93e71fc7898d9770
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-24 15:58:34 +0300

    Automatic message by rover-submit

--- problems/ownership/where_k_th_ordinal_element_greater/src/lib.rs
+++ problems/ownership/where_k_th_ordinal_element_greater/src/lib.rs
@@ -1,6 +1,17 @@
 #![forbid(unsafe_code)]
 
-pub fn where_k_th_ordinal_element_greater(lhs: &Vec<i32>, rhs: &Vec<i32>, k: usize) -> &Vec<i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+pub fn where_k_th_ordinal_element_greater<'a>(
+    lhs: &'a Vec<i32>,
+    rhs: &'a Vec<i32>,
+    k: usize,
+) -> &'a Vec<i32> {
+    let mut tmpl = lhs.clone();
+    let mut tmpr = rhs.clone();
+    tmpl.sort();
+    tmpr.sort();
+    if tmpl[k] <= tmpr[k] {
+        rhs
+    } else {
+        lhs
+    }
 }
