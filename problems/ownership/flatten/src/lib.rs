864f4a86be5066865ddfee12e8ed8e719cafc042
Saprykin Maxim <saprykinoff@inbox.ru>
2023-09-24 12:57:56 +0300

    Automatic message by rover-submit

--- problems/ownership/flatten/src/lib.rs
+++ problems/ownership/flatten/src/lib.rs
@@ -1,11 +1,26 @@
 #![forbid(unsafe_code)]
 
 pub fn flatten<const N: usize>(data: Vec<Box<[&mut i32; N]>>) -> Vec<&mut i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut ans: Vec<&mut i32> = Vec::new();
+    for x in data {
+        for el in *x {
+            ans.push(el)
+        }
+    }
+    ans
 }
 
 pub fn transform_to_fixed_arr<const N: usize>(data: &mut Vec<Vec<i32>>) -> Vec<Box<[&mut i32; N]>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut ans: Vec<Box<[&mut i32; N]>> = Vec::new();
+    for x in data {
+        if x.len() != N {
+            panic!("Inner vectors are of different size");
+        }
+        let mut tmp: Vec<&mut i32> = Vec::new();
+        for el in x {
+            tmp.push(el)
+        }
+        ans.push(Box::new(tmp.try_into().unwrap()));
+    }
+    ans
 }
