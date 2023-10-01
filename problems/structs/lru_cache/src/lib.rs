8fad5f92fa095edbf2a64bdc476350938f823abd
Saprykin Maxim <saprykinoff@inbox.ru>
2023-10-01 10:15:08 +0300

    Automatic message by rover-submit

--- problems/structs/lru_cache/src/lib.rs
+++ problems/structs/lru_cache/src/lib.rs
@@ -5,7 +5,12 @@ use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct LRUCache<K, V> {
-    // TODO: your code goes here.
+    set: BTreeMap<i32, K>,
+    last_use: HashMap<K, i32>,
+    map: HashMap<K, V>,
+    time: i32,
+    capacity: usize,
+    size: usize,
 }
 
 impl<K, V> LRUCache<K, V>
@@ -13,30 +18,77 @@ where
     K: Clone + Hash + Ord,
 {
     pub fn new(capacity: usize) -> Self {
-        // TODO: your code goes here.
+        if capacity == 0 {
+            panic!();
+        }
+        Self {
+            set: BTreeMap::new(),
+            last_use: HashMap::new(),
+            map: HashMap::new(),
+            time: 0,
+            capacity,
+            size: 0,
+        }
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
+        self.size
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
+        self.size == 0
+    }
+
+    fn touch_key(&mut self, key: &K) {
+        if self.last_use.contains_key(key) {
+            let tm = self.last_use[key];
+            self.set.remove(&tm);
+        }
+        self.set.insert(self.time, key.clone());
+        *self.last_use.entry(key.clone()).or_insert(self.time) = self.time;
+        self.time += 1;
     }
 
     pub fn get(&mut self, key: &K) -> Option<&V> {
-        // TODO: your code goes here.
+        if !self.last_use.contains_key(key) {
+            return None;
+        }
+        self.touch_key(key);
+        self.map.get(key)
     }
 
     pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        // TODO: your code goes here.
+        if !self.last_use.contains_key(key) {
+            return None;
+        }
+        self.touch_key(key);
+        self.map.get_mut(key)
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        // TODO: your code goes here.
+        if self.map.contains_key(&key) {
+            self.touch_key(&key);
+            let res = self.map.remove(&key);
+            self.map.insert(key, value);
+            return res;
+        }
+        self.size += 1;
+        self.touch_key(&key);
+        self.map.insert(key, value);
+        if self.size > self.capacity {
+            let a = self.set.pop_first().unwrap();
+            self.map.remove(&a.1);
+            self.last_use.remove(&a.1);
+            self.size -= 1;
+        }
+        None
     }
 
     pub fn clear(&mut self) {
-        // TODO: your code goes here.
+        self.map.clear();
+        self.set.clear();
+        self.last_use.clear();
+        self.time = 0;
+        self.size = 0;
     }
 }
