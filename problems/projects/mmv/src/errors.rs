099f52dc8ec0574573b78abe99e76f6be56a0440
Saprykin Maxim <saprykinoff@inbox.ru>
2023-10-17 00:52:43 +0300

    fix some errors from review

--- problems/projects/mmv/src/errors.rs
+++ problems/projects/mmv/src/errors.rs
@@ -1,18 +1,25 @@
 #![allow(unused)]
 
 use std::path::PathBuf;
-use regex::Error;
+
 
 #[derive(Debug)]
 pub enum MassMoveError {
     RegexError(regex::Error),
+    StdIoError(std::io::Error),
     FileAlreadyExists(PathBuf, PathBuf),
     TemplateMismatch(usize, usize),
     NoFilesFound,
 }
 
 impl From<regex::Error> for MassMoveError {
-    fn from(value: Error) -> Self {
+    fn from(value: regex::Error) -> Self {
         Self::RegexError(value)
+    }
+}
+
+impl From<std::io::Error> for MassMoveError {
+    fn from(value: std::io::Error) -> Self {
+        Self::StdIoError(value)
     }
 }
--- problems/projects/mmv/src/lib.rs
+++ problems/projects/mmv/src/lib.rs
@@ -9,7 +9,7 @@ use crate::file_wrapper::{get_matched_filenames, move_file, split_directory_and_
 use crate::parser::{build_regex, select_data};
 use parser::parse_placeholders;
 
-pub fn insert_data(filename: &str, regex: &str, out: &str) -> Result<String, MassMoveError> {
+pub fn fill_in_output_pattern(filename: &str, regex: &str, out: &str) -> Result<String, MassMoveError> {
     let mut ans = String::new();
     let data = select_data(regex, filename);
     let (placeholders, strings) = parse_placeholders(out);
@@ -29,28 +29,24 @@ pub fn insert_data(filename: &str, regex: &str, out: &str) -> Result<String, Mas
     Ok(ans)
 }
 
-fn output(s: &str) {
-    print!("{}", s);
-}
 
-pub fn mass_move(template: &str, out: &str, force: bool) -> Result<(), MassMoveError> {
-    let (directory_name, file_name) = split_directory_and_file_names(template);
-    let regex = build_regex(&template);
+pub fn mass_move(input_pattern: &str, output_pattern: &str, force_mode: bool) -> Result<(), MassMoveError> {
+    let (directory_name, file_name) = split_directory_and_file_names(input_pattern);
+    let regex = build_regex(&input_pattern);
     let files = get_matched_filenames(&directory_name, &regex);
     if files.is_empty() {
         return Err(MassMoveError::NoFilesFound);
     }
     for file in files {
         let filename = file.into_os_string().into_string().unwrap();
-        let new_filename = insert_data(&filename, &regex, out)?;
-        output(&format!("Moving \"{filename}\" -> \"{new_filename}\" ..."));
+        let new_filename = fill_in_output_pattern(&filename, &regex, output_pattern)?;
+        print!("Moving \"{filename}\" -> \"{new_filename}\" ...");
 
-        if !move_file(&filename, &new_filename, force) {
-            output("Err\n");
-            return Err(MassMoveError::FileAlreadyExists(filename.into(), new_filename.into()));
+        if let Err(error) = move_file(&filename, &new_filename, force_mode) {
+            println!("Error");
+            return Err(error);
         }
-        output("Ok\n");
+        print!("Ok\n");
     }
-
     Ok(())
 }
