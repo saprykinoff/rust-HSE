666eae8309fbecb3516ee28b0226b220066deeea
Saprykin Maxim <saprykinoff@inbox.ru>
2023-10-18 20:57:24 +0300

    made arg parser

--- problems/projects/mmv/src/main.rs
+++ problems/projects/mmv/src/main.rs
@@ -1,11 +1,30 @@
 use mmv_lib;
 use mmv_lib::mass_move;
+use clap::Parser;
+
+/// Mass move utility analog on Rust
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Pattern for files that should be moved. May contain * only in filename. Works only with utf-8 symbols.
+    #[arg(short = 's', long = "src")]
+    source: String,
+
+    /// Pattern for new filenames. May contain #x, where 0 < x <= * count. Works only with utf-8 symbols.
+    #[arg(short = 'd', long = "dst")]
+    destination: String,
+
+    /// Force mode overwrite files if they are already exist.
+    #[arg(short = 'f', long = "force", default_value_t = false)]
+    force_mode: bool,
+}
 
 fn main() {
-    let res = mass_move("playground/*.*", "playground/#2.#1", true);
+    let args = Args::parse();
+    let res = mass_move(&args.source, &args.destination, args.force_mode);
     if res.is_err() {
         println!("{:?}", res)
     }
 
-    //Тут будет парс параметров, обработка help и ошибок
+
 }
