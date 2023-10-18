467be0f9a9a7c6938dd2cbed6296c621421d8fe6
Saprykin Maxim <saprykinoff@inbox.ru>
2023-10-18 20:09:10 +0300

    cargo fmt lol

--- problems/projects/mmv/src/file_wrapper.rs
+++ problems/projects/mmv/src/file_wrapper.rs
@@ -7,7 +7,6 @@ use std::path::{Path, PathBuf};
 ///Splits directory name and file name from path.
 ///Returns pair (directory name, file name)
 pub fn split_directory_and_file_names(path: &str) -> (String, String) {
-
     let res = path.split_once('/');
     if res.is_none() {
         (String::from(""), String::from(path))
@@ -19,15 +18,14 @@ pub fn split_directory_and_file_names(path: &str) -> (String, String) {
 
 ///Scans directory and returns names of files in directory that satisfy regex pattern
 pub fn get_matched_filenames(directory: &str, regex: &str) -> Vec<PathBuf> {
-
     let re = Regex::new(regex).unwrap();
     let files = ScanDir::files().read(directory, |iter| {
         iter.filter(move |(entry, _)| {
             println!("{}", entry.path().into_os_string().to_str().unwrap());
             re.is_match(entry.path().into_os_string().to_str().unwrap())
         })
-            .map(|(entry, _)| entry.path())
-            .collect()
+        .map(|(entry, _)| entry.path())
+        .collect()
     });
     if files.is_err() {
         Vec::new()
@@ -38,7 +36,8 @@ pub fn get_matched_filenames(directory: &str, regex: &str) -> Vec<PathBuf> {
 
 ///Renames file from old_name to new_name.
 ///Returns [`MassMoveError::FileAlreadyExists`] if a path [`new_name`]  is already present in the file system and [`force_mode`] is false
-pub fn move_file(old_name: &str, new_name: &str, force_mode: bool) -> Result<(), MassMoveError> {//TODO replace to PathBuf
+pub fn move_file(old_name: &str, new_name: &str, force_mode: bool) -> Result<(), MassMoveError> {
+    //TODO replace to PathBuf
 
     if old_name == new_name {
         return Ok(());
