690873698a2ff1729f3dfc5ff9366bf7922c1fde
Saprykin Maxim <saprykinoff@inbox.ru>
2023-10-21 16:18:08 +0300

    Automatic message by rover-submit

--- problems/iterators/basics/src/lib.rs
+++ problems/iterators/basics/src/lib.rs
@@ -1,16 +1,26 @@
 #![forbid(unsafe_code)]
 
 pub fn add2(iterator: impl Iterator<Item = i32>) -> impl Iterator<Item = i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    iterator.map(|x| x + 2)
+}
+
+struct Div3Iter {
+    cur: i32,
+}
+
+impl Iterator for Div3Iter {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cur += 3;
+        Some(self.cur)
+    }
 }
 
 pub fn div3() -> impl Iterator<Item = i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    Div3Iter { cur: 0 }
 }
 
 pub fn take_n(iterator: impl Iterator<Item = i32>, n: usize) -> Vec<i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let ans: Vec<_> = iterator.take(n).collect();
+    ans
 }
