2784905a8fcb3f2a2e33bd03607a58f9f2c5fb78
Saprykin Maxim <saprykinoff@inbox.ru>
2023-12-16 14:06:53 +0300

    rewrite test system for more general purpose

--- problems/projects/kafka/tests/tests.rs
+++ problems/projects/kafka/tests/tests.rs
@@ -1,23 +1,24 @@
 #![allow(unused)]
+
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::result;
 use kafka_lib::{read_json, ReceivedJSON};
 use kafka_lib::errors::KafkaError;
 
-fn read_json_wrapper(msg: &str) -> Result<ReceivedJSON, KafkaError> {
+fn get_stream_with_message(msg: &str) -> TcpStream {
     let listener = TcpListener::bind("127.0.0.1:5343").unwrap();
     let mut sender = TcpStream::connect("127.0.0.1:5343").unwrap();
     sender.write(msg.as_bytes()).unwrap();
     let mut stream = listener.accept().unwrap().0;
-    read_json(&mut stream)
+    stream
 }
 
 #[test]
 fn test_read_json_empty() {
-    let result = read_json_wrapper(
-        r#"{}"#
-    ).unwrap();
+    let mut stream = get_stream_with_message(
+        r#"{}"#);
+    let result = read_json(&mut stream).unwrap();
     assert!(result.method.is_none());
     assert!(result.topic.is_none());
     assert!(result.message.is_none());
@@ -26,17 +27,20 @@ fn test_read_json_empty() {
 
 #[test]
 fn test_read_json_corrupted() {
-    let result = read_json_wrapper(
+    let mut stream = get_stream_with_message(
         r#"corrupted"#
     );
+    let result = read_json(&mut stream);
+
     assert!(result.is_err());
 }
 
 #[test]
 fn test_read_json_publisher_register() {
-    let result = read_json_wrapper(
+    let mut stream = get_stream_with_message(
         r#"{"method": "publish", "topic": "topic_name"}"#
-    ).unwrap();
+    );
+    let result = read_json(&mut stream).unwrap();
     assert_eq!(result.method, Some(String::from("publish")));
     assert_eq!(result.topic, Some(String::from("topic_name")));
     assert!(result.message.is_none());
@@ -44,9 +48,10 @@ fn test_read_json_publisher_register() {
 
 #[test]
 fn test_read_json_subscriber_register() {
-    let result = read_json_wrapper(
+    let mut stream = get_stream_with_message(
         r#"{"method": "subscriber", "topic": "topic_name"}"#
-    ).unwrap();
+    );
+    let result = read_json(&mut stream).unwrap();
     assert_eq!(result.method, Some(String::from("subscriber")));
     assert_eq!(result.topic, Some(String::from("topic_name")));
     assert!(result.message.is_none());
@@ -55,9 +60,10 @@ fn test_read_json_subscriber_register() {
 
 #[test]
 fn test_read_json_publisher_post() {
-    let result = read_json_wrapper(
+    let mut stream = get_stream_with_message(
         r#"{"message": "Hello 1 from Rust publisher!"}"#
-    ).unwrap();
+    );
+    let result = read_json(&mut stream).unwrap();
     assert!(result.method.is_none());
     assert!(result.topic.is_none());
     assert_eq!(result.message, Some(String::from("Hello 1 from Rust publisher!")));
@@ -71,4 +77,6 @@ fn test_read_json_all() {
     test_read_json_subscriber_register();
     test_read_json_publisher_post();
 }
+
+
 
